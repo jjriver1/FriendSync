7a01d552becf7053735742a8fa5e43f368abef38
Justin <jmms5757@gmail.com>
2024-04-17 22:49:13 -0400

    added basic post support

--- backend/FriendSync/Controllers/PostController.cs
+++ backend/FriendSync/Controllers/PostController.cs
@@ -4,14 +4,10 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace FriendSync.Controllers;
 
-[ApiController]
+[Controller]
 [Route("api/[controller]")]
-public class PostController : ControllerBase {
-    private readonly PostService _postService;
-
-    public PostController(MongoDBService mongoDBService) {
-        _postService = mongoDBService.PostService;
-    }
+public class PostController(MongoDBService mongoDBService) : Controller {
+    private readonly PostService _postService = mongoDBService.PostService;
 
     [HttpGet]
     public async Task<List<Post>> Get() {
@@ -23,9 +19,17 @@ public class PostController : ControllerBase {
         var post = await _postService.GetPostByIdAsync(id);
         return Ok(post);
     }*/
+    
+    [HttpGet("{author}")]
+    public async Task<List<Post>> GetPostById(string author) {
+        return await _postService.GetPostByAuthorAsync(author);;
+    }
 
     [HttpPost]
     public async Task<IActionResult> CreatePost(Post post) {
+        post.Id = "";
+        post.CreatedAt = DateTime.Now;
+        
         await _postService.CreateAsync(post);
         return CreatedAtAction(nameof(Get), new { id = post.Id }, post);
     }
--- backend/FriendSync/Services/UserService.cs
+++ backend/FriendSync/Services/UserService.cs
@@ -4,26 +4,19 @@ using MongoDB.Driver;
 
 namespace FriendSync.Services; 
 
-public class UserService {
-    
-    private readonly IMongoCollection<User> _userCollection;
-
-    public UserService(IMongoCollection<User> userCollection) {
-        _userCollection = userCollection;
-    }
-    
+public class UserService(IMongoCollection<User> userCollection) {
     public async Task<List<User>> GetAsync() {
-        return await _userCollection.Find(new BsonDocument()).ToListAsync();
+        return await userCollection.Find(new BsonDocument()).ToListAsync();
     }
     
     public async Task<User?> GetUserByIDAsync(string id) {
         FilterDefinition<User> filter = Builders<User>.Filter.Eq("Id", id);
-        return await _userCollection.FindAsync(filter).Result.FirstOrDefaultAsync();
+        return await userCollection.FindAsync(filter).Result.FirstOrDefaultAsync();
     }
     
     public async Task<User?> GetUserByUserNameAsync(string username) {
         FilterDefinition<User> filter = Builders<User>.Filter.Eq("Username", username);
-        return await _userCollection
+        return await userCollection
             .FindAsync(filter)
             .Result
             .FirstOrDefaultAsync();
@@ -31,14 +24,14 @@ public class UserService {
     
     public async Task<User?> GetUserByEmailAsync(string email) {
         FilterDefinition<User> filter = Builders<User>.Filter.Eq("Email", email);
-        return await _userCollection
+        return await userCollection
             .FindAsync(filter)
             .Result
             .FirstOrDefaultAsync();
     }
     
     public async Task<List<User>> GetAllUsersByUserNameAsync(string userNameSubstring) {
-        return await _userCollection
+        return await userCollection
             .FindAsync(user => 
                 user.Username!.Contains(userNameSubstring))
             .Result
@@ -46,19 +39,19 @@ public class UserService {
     }
     
     public async Task CreateAsync(User playlist) {
-        await _userCollection.InsertOneAsync(playlist);
+        await userCollection.InsertOneAsync(playlist);
     }
 
     // TODO: Update
     public async Task AddToUserAsync(string id, string movieId) {
         FilterDefinition<User> filter = Builders<User>.Filter.Eq("Id", id);
         UpdateDefinition<User> update = Builders<User>.Update.AddToSet("movieIds", movieId);
-        await _userCollection.UpdateOneAsync(filter, update);
+        await userCollection.UpdateOneAsync(filter, update);
     }
 
     public async Task<DeleteResult> DeleteAsync(string username) {
         FilterDefinition<User> filter = Builders<User>.Filter.Eq("Username", username);
-        return await _userCollection.DeleteOneAsync(filter);
+        return await userCollection.DeleteOneAsync(filter);
     }
 
 }
